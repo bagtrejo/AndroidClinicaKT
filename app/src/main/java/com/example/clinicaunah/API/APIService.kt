8a86f8c74ddc85f4572984c95ee46994229e55f7
bagtrejo <braslytrejo@gmail.com>
2020-06-13 18:49:41 -0600

    Update APIService.kt

--- app/src/main/java/com/example/clinicaunah/API/APIService.kt
+++ app/src/main/java/com/example/clinicaunah/API/APIService.kt
@@ -1,17 +1,22 @@
 package com.example.clinicaunah.API
 
+import com.example.clinicaunah.Modelos.Login
 import com.example.clinicaunah.Modelos.Paciente
+import com.example.clinicaunah.Modelos.Respuesta
+import org.json.JSONObject
 import retrofit2.Call
-import retrofit2.http.GET
-import retrofit2.http.Url
+import retrofit2.http.*
 
 interface APIService {
 
     @GET("pacientes")
     fun getPacientes():Call<ArrayList<Paciente>>
 
-    @GET("akita/images")
-    fun getCharacterByName(): Call<Paciente>
+    @POST("obtenerUsuario")
+    fun loguear(@Body login: Login): Call<Respuesta>
+
+    @POST("getCurrentUser")
+    fun obtenerUsuario(@Body token: Respuesta): Call<Any>
 
 //    @GET
 //    fun getCharacterByName(@Url url:String): Call<Paciente>
